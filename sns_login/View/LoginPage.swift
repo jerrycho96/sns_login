4a5ae850cab3def88c8cb86973bef016c8682cea
jerrycho96 <suukk2000@naver.com>
2022-01-14 15:03:18 +0900

    push test

--- sns_login/View/LoginPage.swift
+++ sns_login/View/LoginPage.swift
@@ -17,96 +17,97 @@ struct LoginPage: View {
     @AppStorage("userId") var userId: String = ""
 
     var body: some View {
-        NavigationView {
+        VStack {
             VStack {
-                
-                VStack {
-                    
-                    Circle()
-                        .fill(Color.yellow)
-                        .frame(height: 200)
-                        .overlay(Text("피이이인토오오오").foregroundColor(Color.pink))
-                        .padding(.top, 50)
-                    Spacer()
-                    Text("들어올 땐 마음대로지만 나갈 땐 아니라네")
-                   // Text("핀토 로그인")
-                   //     .fontWeight(.bold)
-                   //     .font(.system(size: 30))
-                   //     .foregroundColor(Color.orange)
-                   // .padding()
-                   // .background(
-                   // )
+                Spacer()
+                Circle()
+                    .fill(Color.yellow)
+                    .frame(height: 200)
+                    .overlay(Text("피이이인토오오오").foregroundColor(Color.pink))
+                // padding(.bottom, 100)
+                // .padding(.top, 50)
+                Spacer()
+                Text("들어올 땐 마음대로지만 나갈 땐 아니라네")
+                    .padding(.top, 100)
+                // Text("핀토 로그인")
+                //     .fontWeight(.bold)
+                //     .font(.system(size: 30))
+                //     .foregroundColor(Color.orange)
+                // .padding()
+                // .background(
+                // )
 
 
-                    SignInWithAppleButton(.continue) { request in
-                        request.requestedScopes = [.email, .fullName]
-                    } onCompletion: { result in
+                SignInWithAppleButton(.continue) { request in
+                    request.requestedScopes = [.email, .fullName]
+                } onCompletion: { result in
 
-                        switch result {
-                        case .success(let auth):
-                            switch auth.credential {
-                            case let credential as ASAuthorizationAppleIDCredential:
+                    switch result {
+                    case .success(let auth):
+                        switch auth.credential {
+                        case let credential as ASAuthorizationAppleIDCredential:
 
-                                // User Id
-                                let userId = credential.user
+                            // User Id
+                            let userId = credential.user
 
-                                // User Info
-                                let email = credential.email
-                                let firstName = credential.fullName?.givenName
-                                let lastName = credential.fullName?.familyName
+                            // User Info
+                            let email = credential.email
+                            let firstName = credential.fullName?.givenName
+                            let lastName = credential.fullName?.familyName
 
-                                self.userId = userId
-                                self.email = email ?? ""
-                                self.firstName = firstName ?? ""
-                                self.lastName = lastName ?? ""
+                            self.userId = userId
+                            self.email = email ?? ""
+                            self.firstName = firstName ?? ""
+                            self.lastName = lastName ?? ""
 
-                            default:
-                                break
-                            }
-
-                        case .failure(let error):
-                            print(error)
+                        default:
+                            break
                         }
+
+                    case .failure(let error):
+                        print(error)
                     }
-                        .signInWithAppleButtonStyle(colorScheme == .dark ? .white : .black)
-                        .frame(height: 50)
-                        .padding(.horizontal)
-                        .cornerRadius(8)
-
-                    Button {
-
-                    } label: {
-                        HStack {
-                            Image(systemName: "g.circle.fill")
-                                .resizable()
-                                .renderingMode(.template)
-                                .aspectRatio(contentMode: .fit)
-                                .frame(width: 28, height: 28)
-
-                            Text("Continue with Google")
-                            // .fontWeight(.medium)
-                            .font(.title3)
-                        }
-                            .foregroundColor(Color.green)
-                            .padding()
-                            .frame(maxWidth: .infinity, maxHeight: 50)
-                            .background(
-                            Capsule()
-                                .strokeBorder(Color.green)
-                        )
-                    }
-                    .padding([.leading, .bottom, .trailing])
-                        // .padding()
-                    
-                Text(getAttributedString(string: "Studio Machete jerry"))
                 }
-                    .navigationTitle("핀토 로그인 페이지")
-                    .frame(maxWidth: .infinity, maxHeight: .infinity, alignment: .top)
-                    
+                    .signInWithAppleButtonStyle(colorScheme == .dark ? .white : .black)
+                    .frame(height: 50)
+                    .padding(.horizontal)
+                    .cornerRadius(8)
+
+                Button {
+
+                } label: {
+                    HStack {
+                        Image(systemName: "g.circle.fill")
+                            .resizable()
+                            .renderingMode(.template)
+                            .aspectRatio(contentMode: .fit)
+                            .frame(width: 28, height: 28)
+
+                        Text("Continue with Google")
+                        // .fontWeight(.medium)
+                        .font(.title3)
+                    }
+                        .foregroundColor(Color.green)
+                        .padding()
+                        .frame(maxWidth: .infinity, maxHeight: 50)
+                        .background(
+                        Capsule()
+                            .strokeBorder(Color.green)
+                    )
+                }
+                    .padding([.leading, .bottom, .trailing])
+                // .padding()
+
+                Text(getAttributedString(string: "Studio Machete Jerry"))
             }
-            .background(Color.yellow.opacity(0.1))
-                
+            // .navigationTitle("핀토 로그인 페이지")
+            .frame(maxWidth: .infinity, maxHeight: .infinity, alignment: .top)
+
         }
+            .background(Color.yellow.opacity(0.1))
+        // .frame(maxWidth: .infinity, maxHeight: .infinity, alignment: .top)
+
+
         
     }
 
@@ -116,8 +117,8 @@ struct LoginPage: View {
 
         var attributedString = AttributedString(string)
 
-        if let range = attributedString.range(of: "jerry") {
-            attributedString[range].foregroundColor = .yellow
+        if let range = attributedString.range(of: "Jerry") {
+            attributedString[range].foregroundColor = .red
             attributedString[range].font = .body.bold()
         }
         return attributedString
